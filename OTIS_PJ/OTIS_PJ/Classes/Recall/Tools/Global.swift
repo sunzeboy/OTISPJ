74fb1251740c0dceae84aa72160dc4b5d2c42ef0
sunzeboy <13162153278@163.com>
2017-02-24 15:41:46 +0800

    添加Global文件

---
+++ OTIS_PJ/OTIS_PJ/Classes/Recall/Tools/Global.swift
@@ -0,0 +1,15 @@
+//
+//  Global.swift
+//  OTIS_PJ
+//
+//  Created by sunze on 2017/2/24.
+//  Copyright © 2017年 sunzeboy. All rights reserved.
+//
+
+import UIKit
+
+
+let k_screenW = UIScreen.main.bounds.width
+let k_screenH = UIScreen.main.bounds.height
+
+let bottomViewH:CGFloat = 60.0
--- OTIS_PJ/OTIS_PJ/Classes/Recall/Tools/Protocols.swift
+++ OTIS_PJ/OTIS_PJ/Classes/Recall/Tools/Protocols.swift
@@ -8,11 +8,6 @@
 
 import UIKit
 
-let k_screenW = UIScreen.main.bounds.width
-let k_screenH = UIScreen.main.bounds.height
-
-let bottomViewH:CGFloat = 60.0
-
 
 
 extension UIButton{
