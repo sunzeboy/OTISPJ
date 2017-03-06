a68c0971e32cf9f5f541246ea560c7addb07a29c
duyawei <1843960257@qq.com>
2017-03-06 10:10:20 +0800

    添加helpdesk电话

--- OTIS_PJ/OTIS_PJ/Class/HomePage/Controller/MDAboutVC.m
+++ OTIS_PJ/OTIS_PJ/Class/HomePage/Controller/MDAboutVC.m
@@ -31,16 +31,72 @@
     }];
     
     UILabel* titleLabel=[[UILabel alloc] init];
+//    titleLabel.backgroundColor=[UIColor redColor];
     titleLabel.textColor=[UIColor lightGrayColor];
     titleLabel.textAlignment=NSTextAlignmentCenter;
     titleLabel.font=[UIFont systemFontOfSize:15.0];
-    titleLabel.text=@"Helpdesk电话";
+    titleLabel.text=@"Helpdesk电话:";
     [self.view addSubview:titleLabel];
+    
+    UIButton* phoneLabel=[[UIButton alloc] init];
+    [phoneLabel setTitle:@"姓名1: 15618253221" forState:UIControlStateNormal];
+    [phoneLabel setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    phoneLabel.titleLabel.textAlignment=NSTextAlignmentCenter;
+    [self.view addSubview:phoneLabel];
+    
+    UILabel* phoneLabel1=[[UILabel alloc] init];
+    phoneLabel1.textColor=[UIColor blackColor];
+    phoneLabel1.textAlignment=NSTextAlignmentCenter;
+    phoneLabel1.text=@"姓名2: 15618253221";
+    [phoneLabel1 sizeToFit];
+    phoneLabel1.numberOfLines=0;
+    [self.view addSubview:phoneLabel1];
+    
+    UILabel* phoneLabel2=[[UILabel alloc] init];
+    phoneLabel2.textAlignment=NSTextAlignmentCenter;
+    phoneLabel2.text=@"姓名3: 15618253221";
+    phoneLabel2.textColor=[UIColor blackColor];
+    [phoneLabel2 sizeToFit];
+    phoneLabel2.numberOfLines=0;
+    [self.view addSubview:phoneLabel2];
+    
+    UILabel* phoneLabel3=[[UILabel alloc] init];
+    phoneLabel3.textAlignment=NSTextAlignmentCenter;
+    phoneLabel3.textColor=[UIColor blackColor];
+    phoneLabel3.text=@"姓名4: 15618253221";
+    [phoneLabel3 sizeToFit];
+    phoneLabel3.numberOfLines=0;
+    [self.view addSubview:phoneLabel3];
+    
     
     [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.view.mas_centerX).with.offset(0);
-        make.bottom.equalTo(self.view.mas_bottom).with.offset(-200);
-        make.size.mas_equalTo(CGSizeMake(210, 20));
+        make.bottom.equalTo(self.view.mas_bottom).with.offset(-180);
+        make.size.mas_equalTo(CGSizeMake(100, 20));
+    }];
+    
+    [phoneLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(titleLabel.mas_bottom).with.offset(10);
+        make.centerX.equalTo(self.view.mas_centerX).with.offset(0);
+        make.size.mas_equalTo(CGSizeMake(200, 20));
+    }];
+    
+    [phoneLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(phoneLabel.mas_bottom).with.offset(10);
+        make.centerX.equalTo(self.view.mas_centerX).with.offset(0);
+        make.size.mas_equalTo(CGSizeMake(200, 20));
+    }];
+    
+    [phoneLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(phoneLabel1.mas_bottom).with.offset(10);
+        make.centerX.equalTo(self.view.mas_centerX).with.offset(0);
+        make.size.mas_equalTo(CGSizeMake(200, 20));
+    }];
+    
+    [phoneLabel3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(phoneLabel2.mas_bottom).with.offset(10);
+        make.centerX.equalTo(self.view.mas_centerX).with.offset(0);
+        make.size.mas_equalTo(CGSizeMake(200, 20));
     }];
     
     UIImageView* leftImageView=[[UIImageView alloc] init];
